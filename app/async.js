124f4b4cfd21fcbf20d6ad6cbbb6360ad47eb997
Cristian <cvillegas@baufest.com>
2020-12-13 18:46:14 -0500

    async done

--- app/async.js
+++ app/async.js
@@ -2,10 +2,23 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    const dfd = $.Deferred();
+    setTimeout(function() {
+      dfd.resolve(value);
+    }, 10);
+    return dfd.promise();
   },
 
   manipulateRemoteData: function(url) {
+    const dfd = $.Deferred();
 
+    $.ajax(url).then(function(resp) {
+      const people = $.map(resp.people, function(person) {
+        return person.name;
+      });
+      dfd.resolve(people.sort());
+    });
+
+    return dfd.promise();
   }
 };
