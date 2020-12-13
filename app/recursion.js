5e2e7419803240474d351bad2617c4fdd8d97285
Cristian <cvillegas@baufest.com>
2020-12-13 18:47:48 -0500

    recursion done

--- app/recursion.js
+++ app/recursion.js
@@ -2,7 +2,33 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
+    let listOfFiles = [];
+    let dirs = [];
 
+    function processDir(dir) {
+      let i;
+      let len;
+      let file;
+      let files = dir.files;
+      dirs.push(dir.dir);
+      
+      for (i = 0, len = files.length; i < len; i++) {
+        file = files[i];
+        if (typeof file === 'string') {
+          if (!dirName || dirs.indexOf(dirName) > -1) {
+            listOfFiles.push(files[i]);
+          }
+        } else {
+          processDir(files[i]);
+        }
+      }
+      dirs.pop();
+    }
+
+    processDir(data);
+
+
+    return listOfFiles;
   },
 
   permute: function(arr) {
